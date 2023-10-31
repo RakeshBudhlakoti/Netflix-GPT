94d695f1a8f3bba67056de103e58baa8cfd766c9
Rakesh Budhlakoti <rakeshbudhlakoti1991@gmail.com>
2023-10-31 07:16:40 +0530

    Tailwind CSS Setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="text-3xl font-bol text-green-800 border-lime-600">Hello World!</div>
+    </div>
+  );
+}
+
+export default App;
